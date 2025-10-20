21e027a78c5fee800533e2a6ceb30b9d8f14b56b
zhangjinjian <1016417093@qq.com>
2025-10-20 09:47:48 +0800

    fist commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+  <ThreeScene />
+</template>
+
+<script setup>
+import ThreeScene from './components/ThreeScene.vue'
+</script>
+
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+</style>
